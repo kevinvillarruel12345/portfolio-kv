e168cfd3019e57c5c15d7bbbdb0c7025ad7fe19e
kevin villarruel <kevinevolutivos@gmail.com>
2023-04-06 14:44:00 -0500

    next change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const miBar = document.querySelector('.mi__bar');
 const miManu = document.querySelector('.mi__manu');
 
+
 miBar.addEventListener('click', function() {
   miManu.classList.toggle('open__menu');
 });
+
